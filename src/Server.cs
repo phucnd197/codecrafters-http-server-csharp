ad26222eeb2b437fad369ef58d23cce159904e68
Phuc Nguyen <phuc.nguyen@crmservice.com>
2026-08-24 17:53:00 +0700

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -8,6 +8,8 @@ using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
+var directoryArgStart = Array.IndexOf(args, "--directory");
+var initialFilePath = directoryArgStart != -1 && args.Length > directoryArgStart + 1 ? args[directoryArgStart + 1] : string.Empty;
 
 // TODO: Uncomment the code below to pass the first stage
 using TcpListener server = new(IPAddress.Any, 4221);
@@ -34,43 +36,71 @@ async Task ProcessClientAsync(Socket socket)
             throw new InvalidOperationException("Missing request information");
         }
 
-        var content = string.Empty;
         var (method, path, _) = ParseRequestLine(requestLine);
         var headers = ParseHeaders(header);
         if (method == "GET")
         {
-            content = HandleGetRequest(path, headers);
+            await HandleGetRequest(networkStream, path, headers);
+            return;
         }
 
-        if (string.IsNullOrEmpty(content))
-        {
-            content = "HTTP/1.1 404 Not Found\r\n\r\n";
-        }
-
-        await networkStream.WriteAsync(Encoding.UTF8.GetBytes(content));
+        await networkStream.WriteAsync(Encoding.UTF8.GetBytes("HTTP/1.1 405 Method Not Allowed\r\n\r\n"));
     }
 }
 
-static string HandleGetRequest(string path, Dictionary<string, List<string>> headers)
+async Task HandleGetRequest(NetworkStream networkStream, string path, Dictionary<string, List<string>> headers)
 {
     if (path == "/")
     {
-        return "HTTP/1.1 200 OK\r\n\r\n";
+        await networkStream.WriteAsync(Encoding.UTF8.GetBytes(GetResponse([])));
+        return;
     }
 
     if (path.StartsWith("/echo"))
     {
         var rest = path.Length > 6 ? path.AsSpan().Slice(6) : string.Empty;
-        return $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {rest.Length}\r\n\r\n{rest}";
+        //return $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {rest.Length}\r\n\r\n{rest}";
+        await networkStream.WriteAsync(Encoding.UTF8.GetBytes(GetResponse(rest, contentType: "text/plain")));
+        return;
     }
 
     if (path.StartsWith("/user-agent"))
     {
         var userAgent = headers.GetValueOrDefault("user-agent")?.FirstOrDefault() ?? string.Empty;
-        return $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {userAgent.Length}\r\n\r\n{userAgent}";
+        //return $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {userAgent.Length}\r\n\r\n{userAgent}";
+        await networkStream.WriteAsync(Encoding.UTF8.GetBytes(GetResponse(userAgent, contentType: "text/plain")));
+        return;
     }
 
-    return string.Empty;
+    if (path.StartsWith("/files"))
+    {
+        var fileName = path.Length > 7 ? path.Substring(7) : string.Empty;
+        if (path.Contains(".."))
+        {
+            await networkStream.WriteAsync(Encoding.UTF8.GetBytes(GetResponse([], HttpStatusCode.BadRequest)));
+            return;
+        }
+
+        var fullPath = Path.Combine(initialFilePath, fileName);
+        if (!File.Exists(fullPath))
+        {
+            await networkStream.WriteAsync(Encoding.UTF8.GetBytes(GetResponse([], HttpStatusCode.NotFound, "Not Found")));
+            return;
+        }
+
+        var fileInfo = new FileInfo(fullPath);
+        await networkStream.WriteAsync(Encoding.UTF8.GetBytes(GetResponse([], contentType: "application/octet-stream", contentLength: fileInfo.Length)));
+        using var fileStream = File.OpenRead(fullPath);
+        await fileStream.CopyToAsync(networkStream);
+    }
+}
+
+static string GetResponse(ReadOnlySpan<char> content, HttpStatusCode? status = default, string? statusCode = default, string? contentType = default, long? contentLength = default)
+{
+    var contentTypeHeader = contentType is not null ? $"\r\nContent-Type: {contentType}" : string.Empty;
+    return content.Length == 0
+        ? $"HTTP/1.1 {(int)(status ?? HttpStatusCode.OK)} {status?.ToString() ?? statusCode ?? "OK"}{contentType}\r\n\r\n"
+        : $"HTTP/1.1 {(int)(status ?? HttpStatusCode.OK)} {status?.ToString() ?? statusCode ?? "OK"}{contentType}\r\nContent-Length: {contentLength ?? content.Length}\r\n\r\n{content}";
 }
 
 static RequestLine ParseRequestLine(string requestLine)
